f9fded43a313ab118a8706bd99caa6abaf2e0fc8
heatherpaxton <hello@hjpaxton.com>
2021-07-01 14:34:50 +0100

    Prototype update and card fix

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -26,19 +26,12 @@ module.exports = function (req, res, next) {
       return res.send('<p>Username or password not set in environment variables.</p>');
     }
 
-
-    /* Remove this if we don't want password
-    
     const user = basicAuth(req)
 
     if (!user || user.name !== username || user.pass !== password) {
       res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
       return res.sendStatus(401)
     }
-    
-     up to here! */
-
-    
   }
   next()
 }
